a9f2a390f423060142f40f5c4656a00703070a97
Anders Høgh Strandby Madsen <Andershsm@gmail.com>
2026-09-22 12:54:48 +0200

    fix: change Article.Id from int to Guid

--- src/ArticleService/Models/Article.cs
+++ src/ArticleService/Models/Article.cs
@@ -17,7 +17,7 @@ public enum Region
 }
 public class Article
 {
-    public int Id {get; set;}
+    public Guid Id {get; set;}
     [MaxLength(200)]public required string Title {get; set;}
     [MaxLength(20000)]public required string Content {get; set;}
     [MaxLength(200)]public required string Author {get; set;}
